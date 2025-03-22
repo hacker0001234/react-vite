010b3c962a19f80235e20624318dddd8912932ca
Mykola <mykola.chichkalenko@gmail.com>
2025-03-22 20:30:13 +0200

    uplolad

---
+++ src/main/java/com/example/FrontendBackend/ImageController.java
@@ -0,0 +1,4 @@
+package com.example.FrontendBackend;
+
+public class ImageController {
+}
---
+++ src/main/java/com/example/FrontendBackend/ImageRep.java
@@ -0,0 +1,6 @@
+import com.example.FrontendBackend.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRep extends JpaRepository<Image,Long> {
+
+}
